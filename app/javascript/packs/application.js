cb1bc5e6dee53d6bce6483afd63f6e8acbab41cf
Latteholic <cafelatte.here@gmail.com>
2021-04-13 22:22:26 +0900

    Bootstrapの導入
    下記の記事を参考：
    https://sebdur.medium.com/rails-6-bootstrap-5-16d8cddcba85

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,15 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+require("stylesheets/application.scss")
+window.bootstrap = require("bootstrap");
 
+document.addEventListener('turbolinks:load', ()=> {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+});
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
